2ddc934b10ce5f2d8dfed24dd9c07a89c73f58a3
Tarun Nagaraju <n.tarun2022@gmail.com>
2025-11-08 09:10:56 -0800

    with supabase connect

--- aquashield-frontend/src/main.jsx
+++ aquashield-frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { AuthProvider } from "./context/AuthProvider.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-  <App />
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+);
